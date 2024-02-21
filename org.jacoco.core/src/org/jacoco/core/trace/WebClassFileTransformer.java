aabe6876e0964eb3d96b232c4fb2176333746e98
Zhu Chunzhan, CIH-6-1 <Chunzhan.Zhu@lingyue-digital.com>
2024-02-21 10:06:44 +0800

    clear log

--- org.jacoco.core/src/org/jacoco/core/trace/WebClassFileTransformer.java
+++ org.jacoco.core/src/org/jacoco/core/trace/WebClassFileTransformer.java
@@ -28,7 +28,7 @@ import org.objectweb.asm.Type;
 public class WebClassFileTransformer implements ClassFileTransformer {
 
 	public static final List<String> WEB_SERVER_FILTER = Arrays
-			.asList("org/springframework/web/servlet" + "/DispatcherServlet");
+			.asList("org/springframework/web/servlet/DispatcherServlet");
 	public static final List<String> WEB_SERVER_FILTER_METHOD = Arrays
 			.asList("doDispatch");
 
@@ -37,59 +37,57 @@ public class WebClassFileTransformer implements ClassFileTransformer {
 			Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
 			byte[] classfileBuffer) {
 
-		if (WEB_SERVER_FILTER.contains(className)) {
-			System.out.println("====> Transforming DispatcherServlet");
+		if (!WEB_SERVER_FILTER.contains(className)) {
+			return classfileBuffer;
+		}
+		System.out.println(
+				"====> TraceId inject : Transforming DispatcherServlet ["
+						+ className + "] <====");
 
-			ClassReader cr = new ClassReader(classfileBuffer);
-			ClassWriter cw = new ClassWriter(cr,
-					ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+		ClassReader cr = new ClassReader(classfileBuffer);
+		ClassWriter cw = new ClassWriter(cr,
+				ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+		ClassVisitor cv = new ClassVisitor(Opcodes.ASM7, cw) {
+			@Override
+			public MethodVisitor visitMethod(int access, String name,
+					String descriptor, String signature, String[] exceptions) {
+				MethodVisitor mv = super.visitMethod(access, name, descriptor,
+						signature, exceptions);
 
-			ClassVisitor cv = new ClassVisitor(Opcodes.ASM7, cw) {
-				@Override
-				public MethodVisitor visitMethod(int access, String name,
-						String descriptor, String signature,
-						String[] exceptions) {
-					MethodVisitor mv = super.visitMethod(access, name,
-							descriptor, signature, exceptions);
-
-					// Inject interceptor logic before doDispatch method in
-					// DispatcherServlet
-					if (WEB_SERVER_FILTER_METHOD.contains(name)) {
-						return new MethodVisitor(Opcodes.ASM7, mv) {
-							@Override
-							public void visitCode() {
-								try {
-									// 加载doDispatch方法的第一个参数
-									// 如果varIndex是0，代表org/springframework/web/servlet/DispatcherServlet那个类的this
-									mv.visitVarInsn(Opcodes.ALOAD, 1);
-									mv.visitVarInsn(Opcodes.ALOAD, 2);
-									// 执行MyInterceptor.beforeRequest 方法
-									mv.visitMethodInsn(Opcodes.INVOKESTATIC,
-											Type.getInternalName(
-													WebInterceptor.class),
-											"beforeRequest",
-											Type.getMethodDescriptor(
-													WebInterceptor.class
-															.getMethod(
-																	"beforeRequest",
-																	Object.class,
-																	Object.class)),
-											false);
-								} catch (NoSuchMethodException e) {
-									e.printStackTrace();
-								}
-								super.visitCode();
-							}
-						};
-					}
+				if (!WEB_SERVER_FILTER_METHOD.contains(name)) {
 					return mv;
 				}
-			};
+				// Inject interceptor logic before doDispatch method in
+				// DispatcherServlet
+				return new MethodVisitor(Opcodes.ASM7, mv) {
+					@Override
+					public void visitCode() {
+						try {
+							// doDispatch load params
+							// varIndex 0 is
+							// org/springframework/web/servlet/DispatcherServlet:this
+							mv.visitVarInsn(Opcodes.ALOAD, 1);
+							mv.visitVarInsn(Opcodes.ALOAD, 2);
+							// access MyInterceptor.beforeRequest
+							mv.visitMethodInsn(Opcodes.INVOKESTATIC,
+									Type.getInternalName(WebInterceptor.class),
+									"beforeRequest",
+									Type.getMethodDescriptor(
+											WebInterceptor.class.getMethod(
+													"beforeRequest",
+													Object.class,
+													Object.class)),
+									false);
+						} catch (NoSuchMethodException e) {
+							e.printStackTrace();
+						}
+						super.visitCode();
+					}
+				};
+			}
+		};
 
-			cr.accept(cv, 0);
-			return cw.toByteArray();
-		}
-
-		return classfileBuffer;
+		cr.accept(cv, 0);
+		return cw.toByteArray();
 	}
 }
