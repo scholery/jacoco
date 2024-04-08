2e623bec427ec14c5581270f6fe67b3015249c84
Zhu Chunzhan, CIH-6-1 <Chunzhan.Zhu@lingyue-digital.com>
2024-04-08 11:27:05 +0800

    bytebuffer write

--- org.jacoco.core/src/org/jacoco/core/data/ExecutionDataWriter.java
+++ org.jacoco.core/src/org/jacoco/core/data/ExecutionDataWriter.java
@@ -15,6 +15,8 @@ package org.jacoco.core.data;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UTFDataFormatException;
+import java.nio.ByteBuffer;
 
 import org.jacoco.core.internal.data.CompactDataOutput;
 
@@ -71,9 +73,16 @@ public class ExecutionDataWriter
 	 *             if the header can't be written
 	 */
 	private void writeHeader() throws IOException {
-		out.writeByte(BLOCK_HEADER);
-		out.writeChar(MAGIC_NUMBER);
-		out.writeChar(FORMAT_VERSION);
+		// out.writeByte(BLOCK_HEADER);
+		// out.writeChar(MAGIC_NUMBER);
+		// out.writeChar(FORMAT_VERSION);
+
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		buffer.put(BLOCK_HEADER);
+		buffer.putChar(MAGIC_NUMBER);
+		buffer.putChar(FORMAT_VERSION);
+		out.write(
+				ByteBuffer.wrap(buffer.array(), 0, buffer.position()).array());
 	}
 
 	/**
@@ -88,10 +97,18 @@ public class ExecutionDataWriter
 
 	public void visitSessionInfo(final SessionInfo info) {
 		try {
-			out.writeByte(BLOCK_SESSIONINFO);
-			out.writeUTF(info.getId());
-			out.writeLong(info.getStartTimeStamp());
-			out.writeLong(info.getDumpTimeStamp());
+			// out.writeByte(BLOCK_SESSIONINFO);
+			// out.writeUTF(info.getId());
+			// out.writeLong(info.getStartTimeStamp());
+			// out.writeLong(info.getDumpTimeStamp());
+
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			buffer.put(BLOCK_SESSIONINFO);
+			writeUTF8(buffer, info.getId());
+			buffer.putLong(info.getStartTimeStamp());
+			buffer.putLong(info.getDumpTimeStamp());
+			out.write(ByteBuffer.wrap(buffer.array(), 0, buffer.position())
+					.array());
 		} catch (final IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -100,11 +117,20 @@ public class ExecutionDataWriter
 	public void visitClassExecution(final ExecutionData data) {
 		if (data.hasHits()) {
 			try {
-				out.writeByte(BLOCK_EXECUTIONDATA);
-				out.writeLong(data.getId());
-				out.writeUTF(data.getName());
-				out.writeUTF(data.getTraceId());
-				out.writeBooleanArray(data.getProbes());
+				// out.writeByte(BLOCK_EXECUTIONDATA);
+				// out.writeLong(data.getId());
+				// out.writeUTF(data.getName());
+				// out.writeUTF(data.getTraceId());
+				// out.writeBooleanArray(data.getProbes());
+
+				ByteBuffer buffer = ByteBuffer.allocate(1024);
+				buffer.put(BLOCK_EXECUTIONDATA);
+				buffer.putLong(data.getId());
+				writeUTF8(buffer, data.getName());
+				writeUTF8(buffer, data.getTraceId());
+				writeBooleanArray(buffer, data.getProbes());
+				out.write(ByteBuffer.wrap(buffer.array(), 0, buffer.position())
+						.array());
 			} catch (final IOException e) {
 				throw new RuntimeException(e);
 			}
@@ -129,4 +155,86 @@ public class ExecutionDataWriter
 		return buffer.toByteArray();
 	}
 
+	public int writeUTF8(final ByteBuffer buffer, String str)
+			throws IOException {
+		int strlen = null == str ? 0 : str.length();
+		int utflen = 0;
+		int c, count = 0;
+
+		/* use charAt instead of copying String to char array */
+		for (int i = 0; i < strlen; i++) {
+			c = str.charAt(i);
+			if ((c >= 0x0001) && (c <= 0x007F)) {
+				utflen++;
+			} else if (c > 0x07FF) {
+				utflen += 3;
+			} else {
+				utflen += 2;
+			}
+		}
+
+		if (utflen > 65535)
+			throw new UTFDataFormatException(
+					"encoded string too long: " + utflen + " bytes");
+
+		byte[] bytearr = new byte[utflen + 2];
+
+		bytearr[count++] = (byte) ((utflen >>> 8) & 0xFF);
+		bytearr[count++] = (byte) ((utflen >>> 0) & 0xFF);
+
+		int i = 0;
+		for (i = 0; i < strlen; i++) {
+			c = str.charAt(i);
+			if (!((c >= 0x0001) && (c <= 0x007F)))
+				break;
+			bytearr[count++] = (byte) c;
+		}
+
+		for (; i < strlen; i++) {
+			c = str.charAt(i);
+			if ((c >= 0x0001) && (c <= 0x007F)) {
+				bytearr[count++] = (byte) c;
+
+			} else if (c > 0x07FF) {
+				bytearr[count++] = (byte) (0xE0 | ((c >> 12) & 0x0F));
+				bytearr[count++] = (byte) (0x80 | ((c >> 6) & 0x3F));
+				bytearr[count++] = (byte) (0x80 | ((c >> 0) & 0x3F));
+			} else {
+				bytearr[count++] = (byte) (0xC0 | ((c >> 6) & 0x1F));
+				bytearr[count++] = (byte) (0x80 | ((c >> 0) & 0x3F));
+			}
+		}
+		buffer.put(bytearr, 0, utflen + 2);
+		return utflen + 2;
+	}
+
+	public void writeBooleanArray(final ByteBuffer out, final boolean[] value)
+			throws IOException {
+		writeVarInt(out, value.length);
+		int buffer = 0;
+		int bufferSize = 0;
+		for (final boolean b : value) {
+			if (b) {
+				buffer |= 0x01 << bufferSize;
+			}
+			if (++bufferSize == 8) {
+				out.putInt(buffer);
+				buffer = 0;
+				bufferSize = 0;
+			}
+		}
+		if (bufferSize > 0) {
+			out.putInt(buffer);
+		}
+	}
+
+	public void writeVarInt(final ByteBuffer buffer, final int value)
+			throws IOException {
+		if ((value & 0xFFFFFF80) == 0) {
+			buffer.putInt(value);
+		} else {
+			buffer.putInt(0x80 | (value & 0x7F));
+			writeVarInt(buffer, value >>> 7);
+		}
+	}
 }
